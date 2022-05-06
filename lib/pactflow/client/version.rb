9f8ed49a10134a8e4214e543a8e0814c61388e92
Beth Skurrie <beth@bethesque.com>
2022-05-06 11:35:18 +1000

    chore: initial commit

---
+++ lib/pactflow/client/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pactflow
+  module Client
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/pactflow/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pactflow::Client do
+  it "has a version number" do
+    expect(Pactflow::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
